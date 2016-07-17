76b6da8bb23d7649a9c56ab5f7f4aedbce042dd6
nagytech <nagytech@users.noreply.github.com>
2016-07-17 22:17:36 +1000

    more logic

--- scythe-automa-cards.js
+++ scythe-automa-cards.js
@@ -1,12 +1,40 @@
 var profiles = [{
-  autometta: {
-    type: 'easy',
-    rivers: 5,
-    grid: [0,0,0,0,0,1,1,1,1,1,2,1,1,1,1,2,1,1,2,1,2,1,2,2]
-  }
+  type: 'easy',
+  name: 'Autometta',
+  rivers: 5,
+  phase: 11,
+  grid: [0,0,0,0,0,1,1,1,1,1,2,1,1,1,1,2,1,1,2,1,2,1,2,2]
 }];
+/*
+{
+  id: 0,
+  skip: false,
+  star: false,
+  combat: {
+    spend: [],
+    cards: 0,
+    resources: 0
+  },
+  actions: [{
+    move: [],
+    gets: [],
+    enlist: []
+  },{
+    move: [],
+    gets: [],
+    enlist: []
+  }],
+}
+*/
 var cards = [{
   id: 0,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [6, 7, 7],
+    cards: 1,
+    resources: 1
+  },
   actions: [{
     move: [
       {
@@ -37,11 +65,81 @@ var cards = [{
       quantity: 4
     }],
     enlist: ['power']
-  }],
+  }]
+},{
+  id: 1,
+  skip: false,
+  star: true,
   combat: {
-    spend: [6, 7, 7],
-    cards: 1,
-    resources: 1
+    spend: [5,6,7],
+    cards: 2,
+    resources: 2
   },
-  star: true
+  actions: [{
+    move: [{
+      faction: 'white',
+      type: 'factory'
+    },{
+      type: 'worker'
+    }],
+    gets: [{
+      faction: 'yellow',
+      type: 'gold',
+      quantity: 1
+    },{
+      type: 'gold',
+      quantity: 1
+    }],
+    enlist: []
+  },{
+    move: [{
+      faction: 'white',
+      type: 'factory'
+    },{
+      type: 'worker'
+    }],
+    gets: [{
+      faction: 'yellow',
+      type: 'gold',
+      quantity: 1
+    },{
+      type: 'powercard',
+      quantity: 1
+    },{
+      type: 'character',
+      quantity: 1
+    }],
+    enlist: ['gold']
+  }]
+},{
+  id: 2,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [3,7,7],
+    cards: 0,
+    resources: 0
+  },
+  actions: [{
+    move: [{
+      type: 'worker'
+    }],
+    gets: [{
+      type: 'power',
+      quantity: 3
+    }],
+    enlist: []
+  },{
+    move: [{
+      type: 'worker'
+    }],
+    gets: [{
+      type: 'power',
+      quantity: 2
+    },{
+      type: 'worker',
+      quantity: 1
+    }],
+    enlist: ['popularity']
+  }]
 }];
--- scythe-automa.js
+++ scythe-automa.js
@@ -10,25 +10,14 @@ var fs = {
   }
 };
 
-for (var i = 0; i < cards.length; i++) {
-  console.log(cards[i].id);
-}
-
-var tracker = 0;
-var phase = 0;
-var deck = [];
-var active = {};
-var discards = {
-  main: [],
-  battle: []
-};
+var deck, discards, phase, profile, stars, tracker;
 
 var takeCard = function() {
   if (deck.length == 0) {
     console.debug('shuffling');
     deck = fs.dupe(cards);
   }
-  active = fs.takerandom(deck);
+  return fs.takerandom(deck);
 };
 
 var countMoveActions = function(card, phase) {
@@ -43,26 +32,27 @@ var countEnlistActions = function(card, phase) {
   return countActions(card, phase, 'enlist');
 }
 
-var countActions = function(card, phase, type) {
-  var list = card.actions[phase][type];
+var countActions = function(card, phase, action) {
+  var list = card.actions[phase][action];
   return !list ? 0 : list.length;
 }
 
-var renderEnlistAction = function(i) {
-  return active.actions[phase].enlist[i];
+var renderEnlistAction = function(card, phase, index) {
+  var enlist = card.actions[phase].enlist[index];
+  return enlist ? enlist : 'none';
 };
 
-var renderGetsAction = function(i) {
-  var fac = active.actions[phase].gets[i].faction;
-  var type = active.actions[phase].gets[i].type;
-  var qty = active.actions[phase].gets[i].quantity;
-  fac = fac ? ('If you are ' + fac+ ' faction ') : '';
+var renderGetsAction = function(card, phase, index) {
+  var fac = card.actions[phase].gets[index].faction;
+  var type = card.actions[phase].gets[index].type;
+  var qty = card.actions[phase].gets[index].quantity;
+  fac = fac ? ('If you are ' + fac+ ' faction, gain ') : 'gain ';
   return fac + qty + ' ' + type;
 };
 
-var renderMoveAction = function(i) {
-  var fac = active.actions[phase].move[i].faction;
-  var type = active.actions[phase].move[i].type;
+var renderMoveAction = function(card, phase, index) {
+  var fac = card.actions[phase].move[index].faction;
+  var type = card.actions[phase].move[index].type;
   fac = fac ? ('If you are ' + fac+ ' faction, move ') : 'move ';
   return fac + type;
 };
@@ -73,14 +63,14 @@ var renderActionSet = function(card, phase, counter, render,
   var ij = innerjoin ? innerjoin : '';
   var ct = counter(card, phase) - (innerjoin ? 1 : 0);
   for (var i = 0; i < ct; i++) {
-    ht += render(i) + ij + outerjoin;
+    ht += render(card, phase, i) + ij + outerjoin;
   }
   if (innerjoin)
-    ht += render(ct) + outerjoin;
+    ht += render(card, phase, ct) + outerjoin;
   return ht;
 }
 
-var renderNormal = function() {
+var renderNormal = function(card) {
   // Check to see if we skip this turn
   // Check to see if they can cross rivers?
   // Check to see rank II
@@ -88,38 +78,73 @@ var renderNormal = function() {
   var htmlGets = '';
   var htmlEnlist = '';
 
-  if (active.skip) {
+  if (card.skip) {
     htmlMove = 'Automa skips this round.';
     htmlGets = '';
     htmlEnlist = '';
   } else {
 
-    htmlMove = renderActionSet(active, phase, countMoveActions,
+    htmlMove = renderActionSet(card, phase, countMoveActions,
       renderMoveAction, '<br />', '; otherwise');
-    htmlGets = renderActionSet(active, phase, countGetsActions,
+    htmlGets = renderActionSet(card, phase, countGetsActions,
       renderGetsAction, '<br />');
-    htmlEnlist = renderActionSet(active, phase, countEnlistActions,
+    htmlEnlist = renderActionSet(card, phase, countEnlistActions,
       renderEnlistAction, '', ',');
   }
   document.getElementById('normalCard-actions-move').innerHTML = htmlMove;
   document.getElementById('normalCard-actions-gets').innerHTML = htmlGets;
   document.getElementById('normalCard-actions-enlist').innerHTML = htmlEnlist;
+  document.getElementById('deck-count').innerHTML = deck.length;
+  document.getElementById('card-count').innerHTML = cards.length;
+
+  // TODO: Card count
+
 };
 
 var renderCombat = function() {
-  document.getElementById('combatCard').innerHTML = active.id;
+  //document.getElementById('combatCard').innerHTML = active.id;
 };
 
+var findProfile = function(profiles, type) {
+  return profiles.find(function(profile) {
+    return profile.type == type;
+  });
+};
+
+var startGame = function() {
+  // TODO: Check for game in progress
+  // TODO: Read mode from dropdown
+  tracker = -1;
+  stars = 0;
+  phase = 0;
+  deck = [];
+  discards = {
+    main: [],
+    battle: []
+  };
+  profile = findProfile(profiles, 'easy');
+  console.log(profile);
+  console.log(profiles);
+  document.getElementById('normalCard-actions-move').innerHTML = '';
+  document.getElementById('normalCard-actions-gets').innerHTML = '';
+  document.getElementById('normalCard-actions-enlist').innerHTML = '';
+  document.getElementById('deck-count').innerHTML = cards.length;
+  document.getElementById('card-count').innerHTML = cards.length;
+}
+
 var normalCard = function() {
-  takeCard();
-  renderNormal();
-  discards.main.push(active);
-  active = {};
+  var card = takeCard();
+  tracker++;
+  phase = phase == 1 ? 1 : profile.grid[tracker] == 2 ? 1 : 0;
+  console.log(phase);
+  renderNormal(card);
+  discards.main.push(card);
 };
 
 var combatCard = function() {
-  takeCard();
-  renderCombat();
-  discards.battle.push(active);
-  active = {};
+  var card = takeCard();
+  renderCombat(card);
+  discards.battle.push(card);
 };
+
+startGame();
