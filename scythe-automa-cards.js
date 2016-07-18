fddcc736ffd5bf2fef5afd9276f34cb2865ce1a4
nagytech <nagytech@users.noreply.github.com>
2016-07-18 11:08:28 +1000

    Working copy

--- scythe-automa-cards.js
+++ scythe-automa-cards.js
@@ -1,11 +1,787 @@
 var profiles = [{
-  type: 'easy',
+  t: 'easy',
   name: 'Autometta',
   rivers: 5,
   phase: 11,
   grid: [0,0,0,0,0,1,1,1,1,1,2,1,1,1,1,2,1,1,2,1,2,1,2,2]
+},{
+  t: 'normal',
+  name: 'Automa',
+  rivers: 4,
+  phase: 10,
+  grid: [0,0,0,0,1,1,1,1,1,2,1,1,1,1,2,1,1,2,1,2,2,2]
+},{
+  t: 'hard',
+  name: 'Automaszyna',
+  rivers: 3,
+  phase: 10,
+  grid: [0,0,0,1,1,1,1,1,1,2,1,1,2,1,2,1,2,2,2]
+},{
+  t: 'very hard',
+  name: 'Ultimaszyna',
+  rivers: 0,
+  phase: 9,
+  grid: [1,1,1,1,1,1,1,1,2,1,1,2,1,2,1,2,2,2]
 }];
-/*
+
+var cards = [{
+  id: 0,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [6, 7, 7],
+    cards: 1,
+    resources: 1
+  },
+  actions: [{
+    move: [
+      {
+        t: 'worker',
+        faction: 'blue'
+      },
+      {
+        t: 'factory'
+      },
+      {
+        t: 'character'
+      }
+    ],
+    gets: [{
+      t: 'power',
+      q: 1
+    },{
+      t: 'worker',
+      q: 1
+    }],
+    enlist: ['power']
+  },{
+    move: [{
+      t: 'worker'
+    }],
+    gets: [{
+      t: 'power',
+      q: 4
+    }],
+    enlist: ['power']
+  }]
+},{
+  id: 1,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [5,6,7],
+    cards: 2,
+    resources: 2
+  },
+  actions: [{
+    move: [{
+      faction: 'white',
+      t: 'factory'
+    },{
+      t: 'worker'
+    }],
+    gets: [{
+      faction: 'yellow',
+      t: 'gold',
+      q: 1
+    },{
+      t: 'gold',
+      q: 1
+    }],
+    enlist: []
+  },{
+    move: [{
+      faction: 'white',
+      t: 'factory'
+    },{
+      t: 'worker'
+    }],
+    gets: [{
+      faction: 'yellow',
+      t: 'gold',
+      q: 1
+    },{
+      t: 'powercard',
+      q: 1
+    },{
+      t: 'character',
+      q: 1
+    }],
+    enlist: ['gold']
+  }]
+},{
+  id: 2,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [3,7,7],
+    cards: 0,
+    resources: 0
+  },
+  actions: [{
+    move: [{
+      t: 'worker'
+    }],
+    gets: [{
+      t: 'power',
+      q: 3
+    }],
+    enlist: []
+  },{
+    move: [{
+      t: 'worker'
+    }],
+    gets: [{
+      t: 'power',
+      q: 2
+    },{
+      t: 'worker',
+      q: 1
+    }],
+    enlist: ['popularity']
+  }]
+},{
+  id: 3,
+  skip: true,
+  star: false,
+  combat: {
+    spend: [0,1,2],
+    cards: 1,
+    resources: 0
+  },
+  actions: [{
+    move: [{
+      t: 'worker'
+    }],
+    gets: [{
+      faction: 'yellow',
+      t: 'gold',
+      q: 1
+    },{
+      t: 'powercard'
+    }],
+    enlist: ['powercard']
+  },{
+    move: [{
+      t: 'worker'
+    }],
+    gets: [{
+      faction: 'yellow',
+      t: 'gold',
+      q: 1
+    },{
+      t: 'gold',
+      q: 2
+    }],
+    enlist: ['power']
+  }]
+},{
+  id: 4,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [2,4,7],
+    cards: 1,
+    resources: 4
+  },
+  actions: [{
+    move: [{
+      t: 'worker'
+    }],
+    gets: [{
+      t: 'worker',
+      q: 1
+    },{
+      t: 'gold',
+      q: 1
+    }],
+    enlist: ['gold']
+  },{
+    move: [{
+      t: 'mech'
+    }],
+    gets: [{
+      t: 'power',
+      q: 4
+    },{
+      t: 'worker',
+      q: 1
+    }],
+    enlist: ['powercard']
+  }]
+},{
+  id: 5,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [7,7,7],
+    cards: 1,
+    resources: 0
+  },
+  actions: [{
+    move: [{
+      t: 'worker'
+    }],
+    gets: [{
+      t: 'power',
+      q: 2
+    }],
+    enlist: []
+  },{
+    move: [{
+      t: 'mech'
+    }],
+    gets: [{
+      faction: 'yellow',
+      t: 'gold',
+      q: 1
+    },{
+      t: 'power',
+      q: 2
+    },{
+      t: 'character',
+      q: 1
+    }],
+    enlist: ['power']
+  }]
+},{
+  id: 6,
+  skip: true,
+  star: true,
+  combat: {
+    spend: [5,7,7],
+    cards: 0,
+    resources: 2
+  },
+  actions: [{
+    move: [{
+      faction: 'black',
+      t: 'attack',
+      p: 5
+    },{
+      t: 'worker'
+    }],
+    gets: [{
+      t: 'worker',
+      q: 1
+    },{
+      t: 'gold',
+      q: 1
+    }],
+    enlist: ['popularity']
+  },{
+    move: [{
+      faction: 'black',
+      t: 'attack',
+      p: 5
+    },{
+      t: 'mech'
+    }],
+    gets: [{
+      faction: 'red',
+      t: 'character',
+      q: 1
+    },{
+      t: 'character',
+      q: 1
+    },{
+      t: 'powercard',
+      q: 1
+    }],
+    enlist: ['gold']
+  }]
+},{
+  id: 7,
+  skip: true,
+  star: true,
+  combat: {
+    spend: [0,5,7],
+    cards: 2,
+    resources: 1
+  },
+  actions: [{
+    move: [{
+      t: 'mech'
+    },{
+      t: 'worker'
+    }],
+    gets: [{
+      faction: 'red',
+      t: 'character',
+      q: 1
+    },{
+      t: 'character',
+      q: 1
+    }],
+    enlist: []
+  },{
+    move: [{
+      t: 'mech'
+    }],
+    gets: [{
+      faction: 'black',
+      t: 'power',
+      q: 1
+    },{
+      t: 'gold',
+      q: 1
+    },{
+      t: 'character',
+      q: 1
+    }],
+    enlist: ['gold']
+  }]
+},{
+  id: 8,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [4,7,7],
+    cards: 1,
+    resources: 3
+  },
+  actions: [{
+    move: [{
+      faction: 'white',
+      t: 'factory'
+    },{
+      t: 'mech'
+    },{
+      t: 'worker'
+    }],
+    gets: [{
+      t: 'worker',
+      q: 1
+    },{
+      t: 'character',
+      q: 1
+    }],
+    enlist: ['popularity']
+  },{
+    move: [{
+      faction: 'white',
+      t: 'factory'
+    },{
+      t: 'mech'
+    }],
+    gets: [{
+      t: 'power',
+      q: 3
+    },{
+      t: 'worker',
+      q: 1
+    }],
+    enlist: ['popularity']
+  }]
+},{
+  id: 9,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [0,0,0],
+    cards: 1,
+    resources: 1
+  },
+  actions: [{
+    move: [{
+      t: 'mech'
+    },{
+      t: 'worker'
+    }],
+    gets: [{
+      t: 'character',
+      q: 1
+    },{
+      t: 'power',
+      q: 2
+    }],
+    enlist: []
+  },{
+    move: [{
+      t: 'mech'
+    }],
+    gets: [{
+      t: 'gold',
+      q: 2
+    },{
+      t: 'character',
+      q: 1
+    }],
+    enlist: []
+  }]
+},{
+  id: 10,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [6,7,7],
+    cards: 2,
+    resources: 0
+  },
+  actions: [{
+    move: [{
+      t: 'mech'
+    },{
+      t: 'factory'
+    },{
+      t: 'character'
+    }],
+    gets: [{
+      faction: 'red',
+      t: 'worker',
+      q: 1
+    },{
+      t: 'worker',
+      q: 1
+    },{
+      t: 'gold',
+      q: 1
+    }],
+    enlist: []
+  },{
+    move: [{
+      faction: 'blue',
+      t: 'worker'
+    },{
+      t: 'factory'
+    },{
+      t: 'character'
+    }],
+    gets: [{
+      t: 'power',
+      q: 3
+    },{
+      t: 'worker',
+      q: 1
+    }],
+    enlist: ['powercard']
+  }]
+},{
+  id: 11,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [4,5,7],
+    cards: 2,
+    resources: 1
+  },
+  actions: [{
+    move: [{
+      faction: 'blue',
+      t: 'worker'
+    },{
+      t: 'mech'
+    }],
+    gets: [{
+      t: 'power',
+      q: 4
+    }],
+    enlist: []
+  },{
+    move: [{
+      faction: 'blue',
+      t: 'worker'
+    },{
+      t: 'mech'
+    }],
+    gets: [{
+      t: 'powercard',
+      q: 1
+    },{
+      t: 'gold',
+      q: 1
+    },{
+      t: 'character',
+      q: 1
+    }],
+    enlist: ['power']
+  }]
+},{
+  id: 12,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [3,5,7],
+    cards: 3,
+    resources: 0
+  },
+  actions: [{
+    move: [{
+      faction: 'black',
+      t: 'attack',
+      p: 4
+    },{
+      t: 'factory'
+    },{
+      t: 'character'
+    }],
+    gets: [{
+      t: 'gold',
+      q: 1
+    }],
+    enlist: []
+  },{
+    move: [{
+      faction: 'black',
+      t: 'attack',
+      p: 5
+    },{
+      t: 'factory'
+    },{
+      t: 'character'
+    }],
+    gets: [{
+      t: 'powercard',
+      q: 1
+    },{
+      t: 'gold',
+      q: 2
+    }],
+    enlist: ['gold']
+  }]
+},{
+  id: 13,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [1,1,1],
+    cards: 1,
+    resources: 0
+  },
+  actions: [{
+    move: [{
+      t: 'factory'
+    },{
+      t: 'character'
+    }],
+    gets: [{
+      t: 'worker',
+      q: 1
+    },{
+      t: 'powercard',
+      q: 1
+    }],
+    enlist: ['powercard']
+  },{
+    move: [{
+      t: 'attack',
+      p: 7
+    },{
+      t: 'attack worker'
+    },{
+      t: 'mech'
+    }],
+    gets: [{
+      t: 'power',
+      q: 3
+    }],
+    enlist: ['popularity']
+  }]
+},{
+  id: 14,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [7,7,7],
+    cards: 0,
+    resources: 2
+  },
+  actions: [{
+    move: [{
+      t: 'factory'
+    },{
+      t: 'character'
+    }],
+    gets: [{
+      t: 'power',
+      q: 3
+    },{
+      t: 'gold',
+      q: 1
+    }],
+    enlist: ['gold']
+  },{
+    move: [{
+      t: 'attack',
+      p: 1
+    },{
+      t: 'attack worker'
+    },{
+      t: 'factory'
+    },{
+      t: 'character'
+    }],
+    gets: [{
+      faction: 'yellow',
+      t: 'gold',
+      q: 1
+    },{
+      t: 'gold',
+      q: 1
+    }],
+    enlist: ['powercard']
+  }]
+},{
+  id: 15,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [5,7,7],
+    cards: 1,
+    resources: 4
+  },
+  actions: [{
+    move: [{
+      t: 'factory'
+    },{
+      t: 'character'
+    }],
+    gets: [{
+      t: 'character',
+      q: 1
+    }],
+    enlist: []
+  },{
+    move: [{
+      t: 'attack',
+      p: 5
+    },{
+      t: 'attack worker'
+    },{
+      t: 'mech'
+    }],
+    gets: [{
+      faction: 'black',
+      t: 'power',
+      q: 1
+    },{
+      t: 'character',
+      q: 1
+    },{
+      t: 'gold',
+      q: 1
+    }],
+    enlist: []
+  }]
+},{
+  id: 16,
+  skip: true,
+  star: false,
+  combat: {
+    spend: [7,7,7],
+    cards: 2,
+    resources: 2
+  },
+  actions: [{
+    move: [{
+      t: 'attack',
+      p: 6
+    },{
+      t: 'attack worker'
+    },{
+      t: 'worker'
+    }],
+    gets: [{
+      t: 'gold',
+      q: 2
+    }],
+    enlist: ['power']
+  },{
+    move: [{
+      t: 'attack',
+      p: 8
+    },{
+      t: 'worker'
+    }],
+    gets: [{
+      faction: 'yellow',
+      t: 'gold',
+      q: 1
+    },{
+      t: 'gold',
+      q: 1
+    },{
+      t: 'worker',
+      q: 1
+    }],
+    enlist: ['popularity']
+  }]
+},{
+  id: 17,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [1,1,7],
+    cards: 0,
+    resources: 1
+  },
+  actions: [{
+    move: [{
+      t: 'factory'
+    },{
+      t: 'character'
+    }],
+    gets: [{
+      t: 'worker',
+      q: 1
+    },{
+      t: 'gold',
+      q: 1
+    }],
+    enlist: []
+  },{
+    move: [{
+      t: 'factory'
+    },{
+      t: 'character'
+    }],
+    gets: [{
+      t: 'worker',
+      q: 1
+    },{
+      t: 'gold',
+      q: 2
+    }],
+    enlist: []
+  }]
+},{
+  id: 18,
+  skip: false,
+  star: true,
+  combat: {
+    spend: [7,7,7],
+    cards: 1,
+    resources: 1
+  },
+  actions: [{
+    move: [{
+      t: 'attack worker'
+    },{
+      t: 'worker'
+    }],
+    gets: [{
+      t: 'worker',
+      q: 1
+    }],
+    enlist: []
+  },{
+    move: [{
+      t: 'attack worker'
+    },{
+      t: 'worker'
+    }],
+    gets: [{
+      t: 'power',
+      q: 3
+    },{
+      t: 'gold',
+      q: 1
+    }],
+    enlist: ['powercard']
+  }]
+}];
+
+/* Template
 {
   id: 0,
   skip: false,
@@ -23,123 +799,6 @@ var profiles = [{
     move: [],
     gets: [],
     enlist: []
-  }],
+  }]
 }
 */
-var cards = [{
-  id: 0,
-  skip: false,
-  star: true,
-  combat: {
-    spend: [6, 7, 7],
-    cards: 1,
-    resources: 1
-  },
-  actions: [{
-    move: [
-      {
-        type: 'worker',
-        faction: 'blue'
-      },
-      {
-        type: 'factory'
-      },
-      {
-        type: 'character'
-      }
-    ],
-    gets: [{
-      type: 'power',
-      quantity: 1
-    },{
-      type: 'worker',
-      quantity: 1
-    }],
-    enlist: ['power']
-  },{
-    move: [{
-      type: 'worker'
-    }],
-    gets: [{
-      type: 'power',
-      quantity: 4
-    }],
-    enlist: ['power']
-  }]
-},{
-  id: 1,
-  skip: false,
-  star: true,
-  combat: {
-    spend: [5,6,7],
-    cards: 2,
-    resources: 2
-  },
-  actions: [{
-    move: [{
-      faction: 'white',
-      type: 'factory'
-    },{
-      type: 'worker'
-    }],
-    gets: [{
-      faction: 'yellow',
-      type: 'gold',
-      quantity: 1
-    },{
-      type: 'gold',
-      quantity: 1
-    }],
-    enlist: []
-  },{
-    move: [{
-      faction: 'white',
-      type: 'factory'
-    },{
-      type: 'worker'
-    }],
-    gets: [{
-      faction: 'yellow',
-      type: 'gold',
-      quantity: 1
-    },{
-      type: 'powercard',
-      quantity: 1
-    },{
-      type: 'character',
-      quantity: 1
-    }],
-    enlist: ['gold']
-  }]
-},{
-  id: 2,
-  skip: false,
-  star: true,
-  combat: {
-    spend: [3,7,7],
-    cards: 0,
-    resources: 0
-  },
-  actions: [{
-    move: [{
-      type: 'worker'
-    }],
-    gets: [{
-      type: 'power',
-      quantity: 3
-    }],
-    enlist: []
-  },{
-    move: [{
-      type: 'worker'
-    }],
-    gets: [{
-      type: 'power',
-      quantity: 2
-    },{
-      type: 'worker',
-      quantity: 1
-    }],
-    enlist: ['popularity']
-  }]
-}];
--- scythe-automa.js
+++ scythe-automa.js
@@ -1,3 +1,6 @@
+// TODO: Input power, faction, etc
+// TODO: automatic scoring (need battle resolution though)
+
 var fs = {
   dupe: function(obj) {
     return JSON.parse(JSON.stringify(obj));
@@ -14,7 +17,6 @@ var deck, discards, phase, profile, stars, tracker;
 
 var takeCard = function() {
   if (deck.length == 0) {
-    console.debug('shuffling');
     deck = fs.dupe(cards);
   }
   return fs.takerandom(deck);
@@ -45,17 +47,21 @@ var renderEnlistAction = function(card, phase, index) {
 var renderGetsAction = function(card, phase, index) {
   var pac = card.actions[phase].gets[index];
   var fac = pac.faction;
-  var type = pac.type;
-  var qty = pac.quantity;
-  fac = fac ? ('If you are ' + fac+ ' faction, gain ') : 'gain ';
+  var type = pac.t;
+  var qty = pac.q;
+  fac = fac ? (fac + ' faction gain ') : 'gain ';
   return fac + qty + ' ' + type;
 };
 
 var renderMoveAction = function(card, phase, index) {
   var pac = card.actions[phase].move[index];
   var fac = pac.faction;
-  var type = pac.type;
-  fac = fac ? ('If you are ' + fac+ ' faction, move ') : 'move ';
+  var type = pac.t;
+  fac = fac ? (fac + ' faction ') : '';
+  if (type == 'attack')
+    type = 'attack (req. ' + pac.power + ' power)';
+  if (type == 'factory')
+    type = '<b>character</b> towards <b>fac/obj</b>';
   return fac + type;
 };
 
@@ -73,20 +79,15 @@ var renderActionSet = function(card, phase, counter, render,
 }
 
 var renderNormal = function(card) {
-  // Check to see if we skip this turn
-  // Check to see if they can cross rivers?
-  // Check to see rank II
   var htmlMove = '';
   var htmlGets = '';
   var htmlEnlist = '';
 
-  if (card.skip) {
-    htmlMove = 'Automa skips this round.';
-    htmlGets = '';
-    htmlEnlist = '';
+  if (card.skip && profile.type == 'easy') {
+    htmlEnlist = htmlGets = htmlMove = 'Automa skips this round.';
   } else {
     htmlMove = renderActionSet(card, phase, countMoveActions,
-      renderMoveAction, '<br />', '; otherwise');
+      renderMoveAction, '<br />', '; or');
     htmlGets = renderActionSet(card, phase, countGetsActions,
       renderGetsAction, '<br />');
     htmlEnlist = renderActionSet(card, phase, countEnlistActions,
@@ -101,19 +102,67 @@ var renderNormal = function(card) {
 
 };
 
-var renderCombat = function() {
-  //document.getElementById('combatCard').innerHTML = active.id;
+var renderCombat = function(card) {
+  var htmlCombat = '<table class="table is-narrow"><thead><tr>';
+  var arr = ['0-7', '8-13', '14+', 'P', 'R'];
+  for (var i = 0; i < arr.length; i++) {
+    htmlCombat += '<th>' + arr[i] + '</th>'
+  }
+  htmlCombat += '</tr></thead><tbody><tr>';
+  if (card) {
+    var cc = card.combat;
+    for (var i = 0; i < 3; i++) {
+      htmlCombat += '<td>' + cc.spend[i] + '</td>'
+    }
+    htmlCombat += '<td>' + cc.cards + '</td>';
+    htmlCombat += '<td>' + cc.resources + '</td>';
+  } else {
+    for (var i = 0; i < 5; i++) {
+      htmlCombat += '<td></td>';
+    }
+  }
+  '</tr></tbody></table>';
+  document.getElementById('combat-card-actions').innerHTML = htmlCombat;
 };
 
 var findProfile = function(profiles, type) {
   return profiles.find(function(profile) {
-    return profile.type == type;
+    return profile.t == type;
   });
 };
 
+var setCardPhase = function(phase) {
+  var acards = document.getElementsByClassName('acard');
+  for (var i = 0; i < acards.length; i++) {
+    acards[i].className = 'acard message is-' + (phase == 1 ? 'danger' : 'success');
+  }
+}
+
+var setStars = function(stars) {
+  var sts = '';
+  for (var i = 6; i > 0; i--) {
+    sts += ' ' + (i > stars ? '&#9734;' : '&#9733;');
+  }
+  document.getElementById('stars').innerHTML = sts;
+}
+
+var selectDifficulty = function() {
+  if (tracker > -1 || (stars > 0 && stars < 6))
+  if (!confirm('If you change the difficulty, the game will reset.'))
+    return false;
+  resetGame();
+  return true;
+};
+
 var startGame = function() {
-  // TODO: Check for game in progress
-  // TODO: Read mode from dropdown
+  resetGame();
+  document.getElementById('start').setAttribute('disabled', 'disabled');
+  document.getElementById('difficulty').setAttribute('disabled', 'disabled');
+  document.getElementById('playcard').removeAttribute('disabled');
+  document.getElementById('playcombatcard').removeAttribute('disabled');
+}
+
+var resetGame = function() {
   tracker = -1;
   stars = 0;
   phase = 0;
@@ -122,26 +171,43 @@ var startGame = function() {
     main: [],
     battle: []
   };
-  profile = findProfile(profiles, 'easy');
-  console.log(profile);
-  console.log(profiles);
+  var difficulty = document.getElementById('difficulty').value;
+  profile = findProfile(profiles, difficulty);
   document.getElementById('normalCard-actions-move').innerHTML = '';
   document.getElementById('normalCard-actions-gets').innerHTML = '';
   document.getElementById('normalCard-actions-enlist').innerHTML = '';
-  document.getElementById('phase').innerHTML = 'I';
+  document.getElementById('phase').innerHTML = 'Phase I';
+  document.getElementById('river-state').innerHTML = 'cannot';
   document.getElementById('deck-count').innerHTML = cards.length;
   document.getElementById('card-count').innerHTML = cards.length;
+  setStars(stars);
+  setCardPhase(phase);
+  document.getElementById('start').removeAttribute('disabled');
+  document.getElementById('difficulty').removeAttribute('disabled');
 }
 
 var normalCard = function() {
+  if (stars >= 6) {
+    return false;
+  }
   var card = takeCard();
   tracker++;
-  phase = phase == 1 ? 1 : profile.grid[tracker] == 2 ? 1 : 0;
-  document.getElementById('phase').innerHTML = (phase == 0) ? 'I' : 'II';
-  // TODO: End game
-  // TODO: Chane color of cards
+  var sa = profile.grid[tracker];
+  phase = phase == 1 ? 1 : sa == 2 ? 1 : 0;
+  if (sa == 2) stars++;
+  setStars(stars);
+  setCardPhase(phase);
+  document.getElementById('phase').innerHTML = (phase == 0) ? 'Phase I' : 'Phase II';
+  document.getElementById('river-state').innerHTML = tracker > profile.rivers ? 'can' : 'cannot';
   renderNormal(card);
+  renderCombat();
   discards.main.push(card);
+  if (stars >= 6) {
+    document.getElementById('phase').innerHTML = 'Game Over';
+    document.getElementById('playcard').setAttribute('disabled', 'disabled');
+    document.getElementById('playcombatcard').setAttribute('disabled', 'disabled');
+    return;
+  }
 };
 
 var combatCard = function() {
@@ -150,4 +216,4 @@ var combatCard = function() {
   discards.battle.push(card);
 };
 
-startGame();
+resetGame();
